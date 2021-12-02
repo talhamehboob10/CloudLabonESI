221792fe7cefb6cd6e42810a75baa65320faf3c5
achauhan06 <60124910+achauhan06@users.noreply.github.com>
2021-12-02 16:29:55 -0500

    Update Node.pm

--- emulab-devel-new/emulab-devel/build_aish/db/Node.pm
+++ emulab-devel-new/emulab-devel/build_aish/db/Node.pm
@@ -132,6 +132,8 @@ sub Lookup($$)
 {
     my ($class, $token) = @_;
     my $nodeid;
+    
+    print "token in node file : $token\n";
 
     if ($token =~ /^\w{8}\-\w{4}\-\w{4}\-\w{4}\-\w{12}$/) {
 	my $query_result =
