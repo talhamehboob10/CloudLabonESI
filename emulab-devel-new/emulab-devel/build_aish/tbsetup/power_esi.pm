e444631c5050de2c81152cb69dd74fa15f1be175
achauhan06 <60124910+achauhan06@users.noreply.github.com>
2021-12-02 11:59:29 -0500

    Update power_esi.pm

--- emulab-devel-new/emulab-devel/build_aish/tbsetup/power_esi.pm
+++ emulab-devel-new/emulab-devel/build_aish/tbsetup/power_esi.pm
@@ -3,6 +3,18 @@ print "Hello World\n";
 
 use REST::Client;
 
+sub new($$$;$$) {
+
+}
+
+sub status($$$;$$) {
+
+}
+
+sub power($$$;$$) {
+
+}
+
 my $client = REST::Client->new();
 $client->GET('https://618857b5057b9b00177f9c43.mockapi.io/esi/esimock');
 print $client->responseContent();
