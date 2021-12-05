a35f349e513baf0a7d81ffcb3a77c4f64dd13716
achauhan06 <60124910+achauhan06@users.noreply.github.com>
2021-12-05 03:44:53 -0500

    Update power_esi.pm

--- emulab-devel-new/emulab-devel/build_aish/tbsetup/power_esi.pm
+++ emulab-devel-new/emulab-devel/build_aish/tbsetup/power_esi.pm
@@ -53,7 +53,7 @@ sub status {
    my $url = "https://mockesi.herokuapp.com/status/$devicename";
    $client->GET($url);
    print $url;
-   my $response = $client->responseContent();
+   my @response = @{JSON::XS::decode_json($client->responseContent())};
    
    
    #my $device_status = $hashStatus->{$devicename};
