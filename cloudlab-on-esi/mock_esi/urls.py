3bbec25e2b02d16c8f9d944f70c010a504fc5974
Aishwarya Chauhan <chauhan.ai@husky.neu.edu>
2021-12-05 00:46:10 -0500

    Added power on off

--- cloudlab-on-esi/mock_esi/urls.py
+++ cloudlab-on-esi/mock_esi/urls.py
@@ -1,11 +1,15 @@
-from django.urls import path
-from .views import index, nodeCommands, index_terminal
+from django.urls import path, include
+from .views import index, nodeCommands, index_terminal, powerOn, powerOff, status
+
 
 urlpatterns = [
-     path('', index, name='home'),
+    path('', index, name='home'),
     path('view/<int:id>/', nodeCommands, name='node'),
     path('terminal/', index_terminal, name='index_terminal'),
-
+    path('powerOn/<int:id>/', powerOn, name="powerOn"),
+    path('powerOff/<int:id>/', powerOff, name="powerOff"),
+    path('status/<int:id>/', status, name="status"),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 
 
 ]
--- cloudlab-on-esi/mock_esi/views.py
+++ cloudlab-on-esi/mock_esi/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 import requests
-
+from rest_framework.decorators import api_view
+from .serializers import NodeSerializer, MessageSerializer
+from rest_framework.views import Response
+import json
 
 def index(request):
     response = requests.get('https://618857b5057b9b00177f9c43.mockapi.io/esi/esimock').json()
@@ -17,3 +20,49 @@ def nodeCommands(request, id):
 
 def index_terminal(request):
     return render(request, 'index_terminal.html')
+
+@api_view(['GET'])
+def status(request, id):
+    node = getNode(id);
+    if node['nodeStatus'] == "False":
+        message = "The node is switched off"
+    else:
+        message = "The node is switched on"
+    result = {"message": message}
+    response = MessageSerializer(result, many=False).data
+    return Response(response)
+
+@api_view(['GET'])
+def powerOn(request, id):
+    node = getNode(id);
+    if node['nodeStatus'] == "False":
+        message = "The node has been is switched on."
+    else:
+        message = "The node is already switched on."
+    result = {"message": message}
+    response = MessageSerializer(result, many=False).data
+    return Response(response)
+
+@api_view(['GET'])
+def powerOff(request, id):
+    node = getNode(id);
+    if node['nodeStatus'] == "False":
+        message = "The node is already switched off."
+    else:
+        message = "The node has been switched off."
+    result = {"message": message}
+    response = MessageSerializer(result, many=False).data
+    return Response(response)
+
+def getNode(id):
+    response = requests.get('https://618857b5057b9b00177f9c43.mockapi.io/esi/esimock').json()
+    data = json.loads(json.dumps(response))
+    for i in data:
+        if i['nodeID'] == str(id):
+            node = {
+                    "nodeName": i['nodeName'],
+                    "nodeStatus": str(i['nodeStatus']),
+                    "nodeID": i['nodeID'],
+                }
+            break;
+    return node;
