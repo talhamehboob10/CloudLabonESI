3ace5577e1febf257f66d86d5a3e6ccfbe9c8c6f
nairsneha <nair.sneha1997@gmail.com>
2021-12-04 19:53:01 -0500

    trying to update mock esi api

--- cloudlab-on-esi/mock_esi/urls.py
+++ cloudlab-on-esi/mock_esi/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from .views import index, nodeCommands, index_terminal
+from .views import index, nodeCommands, index_terminal, powerOn
 
 urlpatterns = [
-     path('', index, name='home'),
+    path('', index, name='home'),
     path('view/<int:id>/', nodeCommands, name='node'),
     path('terminal/', index_terminal, name='index_terminal'),
-
-
+    path('view/poweron/<int:id>/', powerOn, name='poweron')
 
 ]
